dbed17d99a08008458ccc46264248d6dff7d146d
Ulvenforst <ulvenforst@gmail.com>
2023-12-17 09:37:20 -0500

    Se completó la tarea #5 y #6, capturando el comando ejecutado en el servidor y mostrándolo en el cliente

--- cliente/cliente.c
+++ cliente/cliente.c
@@ -1,8 +1,8 @@
 /*
 Archivo: cliente.c
 Autor: Juan Camilo Narváez Tascón <juan.narvaez.tascon@correounivalle.edu.co>
-       Juan Esteban Clavijo García <juan.esteban.clavijo@correounivalle.edu.co>
-       Óscar David Cuaical <cuaical.oscar@correounivalle.edu.co>
+Juan Esteban Clavijo García <juan.esteban.clavijo@correounivalle.edu.co>
+Óscar David Cuaical <cuaical.oscar@correounivalle.edu.co>
 Fecha creación: 14-12-23
 Fecha última modificación: 14-12-23
 Licencia: GPL-3.0
@@ -33,80 +33,78 @@ int establecer_conexion(char *host, t_port port);
 void interactuar_con_servidor(int sockfd);
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Uso: %s <host> <puerto>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+  if (argc != 3) {
+    fprintf(stderr, "Uso: %s <host> <puerto>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
 
-    // Convertir y validar el puerto
-    t_port port = atoi(argv[2]);
-    validar_puerto(port);
+  // Convertir y validar el puerto
+  t_port port = atoi(argv[2]);
+  validar_puerto(port);
 
-    // Establecer la conexión con el servidor
-    int sockfd = establecer_conexion(argv[1], port);
+  // Establecer la conexión con el servidor
+  int sockfd = establecer_conexion(argv[1], port);
 
-    // Interactuar con el servidor
-    interactuar_con_servidor(sockfd);
+  // Interactuar con el servidor
+  interactuar_con_servidor(sockfd);
 
-    // Cerrar la conexión
-    if (TCP_Close(sockfd) < 0) {
-        fprintf(stderr, "Error al cerrar el socket.\n");
-        exit(EXIT_FAILURE);
-    }
+  // Cerrar la conexión
+  if (TCP_Close(sockfd) < 0) {
+    fprintf(stderr, "Error al cerrar el socket.\n");
+    exit(EXIT_FAILURE);
+  }
 
-    exit(EXIT_SUCCESS);
+  exit(EXIT_SUCCESS);
 }
 
 // Implementaciones de funciones
 void validar_puerto(t_port port) {
-    if (port < MIN_PORT || port > MAX_PORT) {
-        fprintf(stderr, "Puerto fuera de rango. Utiliza un puerto entre %d y %d.\n", MIN_PORT, MAX_PORT);
-        exit(EXIT_FAILURE);
-    }
+  if (port < MIN_PORT || port > MAX_PORT) {
+    fprintf(stderr, "Puerto fuera de rango. Utiliza un puerto entre %d y %d.\n", MIN_PORT, MAX_PORT);
+    exit(EXIT_FAILURE);
+  }
 }
 
 int establecer_conexion(char *host, t_port port) {
-    printf("Intentando conectar en <%s,%d>\n", host, port);
-    int sockfd = TCP_Open(Get_IP(host), port);
-    if (sockfd < 0) {
-        fprintf(stderr, "Error al abrir el socket TCP.\n");
-        exit(EXIT_FAILURE);
-    }
-    return sockfd;
+  printf("Intentando conectar en <%s,%d>\n", host, port);
+  int sockfd = TCP_Open(Get_IP(host), port);
+  if (sockfd < 0) {
+    fprintf(stderr, "Error al abrir el socket TCP.\n");
+    exit(EXIT_FAILURE);
+  }
+  return sockfd;
 }
 
 void interactuar_con_servidor(int sockfd) {
-    char cmd[BUFSIZ];
+  char cmd[BUFSIZ];
 
-    printf("Conectado al servidor. Escribe 'salir' para terminar.\n");
-    while(1) {
-        printf("> ");
-        fflush(stdout);
-        leer_de_teclado(BUFSIZ, cmd);
+  printf("Conectado al servidor. Escribe 'salir' para terminar.\n");
+  while(1) {
+    printf("> ");
+    fflush(stdout);
+    leer_de_teclado(BUFSIZ, cmd);
 
-        // Si el comando es "salir", terminar el bucle
-        if (strcmp(cmd, "salir") == 0) {
-            break;
-        }
-
-        // se crea un nuevo proceso para enviar el comando al servidor
-        pid_t pid = fork();
-        if (pid < 0) {
-            fprintf(stderr, "Fork failed");
-            exit(EXIT_FAILURE);
-        }
-        // Proceso hijo
-        if (pid == 0) { 
-            // Envia el comando al servidor
-            TCP_Write_String(sockfd, cmd);
-            Recv_ACK(sockfd);
-            exit(EXIT_SUCCESS);
-        } 
-        else //proceso padre
-        { 
-            // Esperar a que el proceso hijo termine
-            int status;
-            waitpid(pid, &status, 0);
-        }
+    if (strcmp(cmd, "salir") == 0) {
+      break;
     }
+
+    pid_t pid = fork();
+    if (pid < 0) {
+      fprintf(stderr, "Fork failed");
+      exit(EXIT_FAILURE);
+    }
+    if (pid == 0) {  // Proceso hijo
+      TCP_Write_String(sockfd, cmd);
+      Recv_ACK(sockfd);
+      exit(EXIT_SUCCESS);
+    } else {  // Proceso padre
+      int status;
+      waitpid(pid, &status, 0);
+      // Leer la respuesta del servidor
+      char respuesta[BUFSIZ];
+      bzero(respuesta, BUFSIZ);
+      TCP_Read_String(sockfd, respuesta, BUFSIZ);
+      printf("%s\n", respuesta);
+    }
+  }
 }
--- servidor/servidor.c
+++ servidor/servidor.c
@@ -1,8 +1,8 @@
 /*
 Archivo: servidor.c
 Autor: Juan Camilo Narváez Tascón <juan.narvaez.tascon@correounivalle.edu.co>
-        Juan Esteban Clavijo García <juan.esteban.clavijo@correounivalle.edu.co>
-        Óscar David Cuaical <cuaical.oscar@correounivalle.edu.co>
+Juan Esteban Clavijo García <juan.esteban.clavijo@correounivalle.edu.co>
+Óscar David Cuaical <cuaical.oscar@correounivalle.edu.co>
 Fecha creación: 14-12-23
 Fecha última modificación: 14-12-23
 Licencia: GPL-3.0
@@ -32,98 +32,86 @@ void manejar_cliente(int connfd);
 
 int main(int argc, char *argv[])
 {
-    int sockfd, connfd;
-    t_port puerto;
+  int sockfd, connfd;
+  t_port puerto;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "Uso: %s <puerto>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+  if (argc != 2)
+  {
+    fprintf(stderr, "Uso: %s <puerto>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
 
-    puerto = atoi(argv[1]);
-    validar_puerto(puerto);
+  puerto = atoi(argv[1]);
+  validar_puerto(puerto);
 
-    sockfd = iniciar_servidor(puerto);
-    connfd = TCP_Accept(sockfd); // Aceptar la conexión del cliente
+  sockfd = iniciar_servidor(puerto);
+  connfd = TCP_Accept(sockfd); // Aceptar la conexión del cliente
 
-    manejar_cliente(connfd); // Manejar la interacción con el cliente
+  manejar_cliente(connfd); // Manejar la interacción con el cliente
 
-    close(sockfd); // Cerrar el socket del servidor
-    return 0;
+  close(sockfd); // Cerrar el socket del servidor
+  return 0;
 }
 
 // Implementaciones de funciones
 void validar_puerto(t_port port)
 {
-    if (port < MIN_PORT || port > MAX_PORT)
-    {
-        fprintf(stderr, "Puerto fuera de rango. Utiliza un puerto entre %d y %d.\n", MIN_PORT, MAX_PORT);
-        exit(EXIT_FAILURE);
-    }
+  if (port < MIN_PORT || port > MAX_PORT)
+  {
+    fprintf(stderr, "Puerto fuera de rango. Utiliza un puerto entre %d y %d.\n", MIN_PORT, MAX_PORT);
+    exit(EXIT_FAILURE);
+  }
 }
 
 int iniciar_servidor(t_port port)
 {
-    int socket = TCP_Server_Open(port);
-    if (socket < 0)
-    {
-        fprintf(stderr, "Error al abrir el socket del servidor.\n");
-        exit(EXIT_FAILURE);
-    }
-    return socket;
+  int socket = TCP_Server_Open(port);
+  if (socket < 0)
+  {
+    fprintf(stderr, "Error al abrir el socket del servidor.\n");
+    exit(EXIT_FAILURE);
+  }
+  return socket;
 }
 
-void manejar_cliente(int connfd)
-{
-    char buff[BUFSIZ];
+void manejar_cliente(int connfd) {
+  char buff[BUFSIZ];
 
-    printf("Esperando comandos del cliente...\n");
-    while (1)
-    {
-        bzero(buff, BUFSIZ); // Limpiar buffer antes de cada lectura
+  printf("Esperando comandos del cliente...\n");
+  while (1) {
+    bzero(buff, BUFSIZ);
 
-        // Leer mensaje del cliente
-        int n = TCP_Read_String(connfd, buff, BUFSIZ);
+    int n = TCP_Read_String(connfd, buff, BUFSIZ);
 
-        if (n <= 0)
-        {
-            printf("Cliente desconectado o error de lectura.\n");
-            break;
-        }
-        printf("Comando recibido del cliente: %s\n", buff);
-
-        Send_ACK(connfd);
-
-        // Se crea un nuevo proceso para ejecutar el comando recibido
-        pid_t pid = fork();
-        if (pid < 0)
-        {
-            fprintf(stderr, "Fork failed");
-            exit(EXIT_FAILURE);
-        }
-        // Proceso hijo
-        if (pid == 0)
-        {
-            char **vector;
-            vector = de_cadena_a_vector(buff);
-            // Se ejecuta el comando
-            execvp(vector[0], vector);
-            exit(EXIT_SUCCESS);
-        }
-        else // proceso padre
-        {
-            // Esperar a que el proceso hijo termine
-            wait(NULL);
-        }
-        // ...
-
-        // Verificar si el comando es para cerrar la conexión
-        if (strcmp(buff, "salir") == 0)
-        {
-            printf("Comando para terminar la conexión recibido.\n");
-            break;
-        }
-        // Aquí se enviaría la respuesta al cliente
+    if (n <= 0) {
+      printf("Cliente desconectado o error de lectura.\n");
+      break;
     }
+    printf("Comando recibido del cliente: %s\n", buff);
+
+    Send_ACK(connfd);
+
+    pid_t pid = fork();
+    if (pid < 0) {
+      fprintf(stderr, "Fork failed");
+      exit(EXIT_FAILURE);
+    }
+    if (pid == 0) {
+      char **vector = de_cadena_a_vector(buff);
+
+      dup2(connfd, STDERR_FILENO);
+      dup2(connfd, STDOUT_FILENO);
+
+      execvp(vector[0], vector);
+      fprintf(stderr, "Error al ejecutar '%s'\n", buff);
+      exit(EXIT_FAILURE);
+    } else {
+      wait(NULL);
+    }
+
+    if (strcmp(buff, "salir") == 0) {
+      printf("Comando para terminar la conexión recibido.\n");
+      break;
+    }
+  }
 }
